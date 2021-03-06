8c4803cf06cbe37c4fc4a6c6e81cba158263dc4d
AP <porwal.1@iitj.ac.in>
2021-03-06 21:01:50 +0530

    Review Adding Route added

--- routes/api/product.js
+++ routes/api/product.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const auth = require("../../middlewares/auth");
 const upload = require("../../utils/upload").single('image');
 const Product = require("../../models/Product");
+const { check, validationResult } = require('express-validator');
 
 //@Route    GET /products/get
 //@desc     Get All Products
@@ -21,7 +22,12 @@ router.get("/get", auth, async (req, res) => {
 router.get("/get/:id", auth, async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
-        return res.status(200).json(product);
+        if (product) {
+            return res.status(200).json(product);
+        }
+        else {
+            return res.status(404).json({ errors: [{ msg: 'Product Not Found' }] });
+        }
     } catch (err) {
         if (err.kind === 'ObjectId') {
             return res.status(404).json({ errors: [{ msg: 'Product Not Found' }] });
@@ -103,5 +109,75 @@ router.patch("/update/:id", auth, async (req, res) => {
     }
 })
 
+//@Route    DELETE /products/delete/:id
+//@desc     Delete a Product
+//@access   Private
+router.delete("/delete/:id", auth, async (req, res) => {
+    const user = req.user;
+    if (user.type === "admin") {
+        try {
+            const product = await Product.findById(req.params.id);
+            if (product) {
+                await Product.findByIdAndDelete(req.params.id);
+                return res.status(200).json({ msg: 'Product Deleted' });
+            }
+            else {
+                return res.status(404).json({ errors: [{ msg: 'Product Not Found' }] });
+            }
+        } catch (err) {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).json({ errors: [{ msg: 'Product Not Found' }] });
+            }
+            return res.status(500).json({ errors: [{ msg: 'Internal Server Error' }] });
+        }
+    }
+    else {
+        return res.status(400).json({ errors: [{ msg: 'Not Authorised' }] });
+    }
+})
 
+//@Route    PUT /products/review/:id
+//@desc     Giving review to a product by ID
+//@access   Private
+router.put("/review/:id", [
+    auth,
+    check('comment', 'Comment is required').not().isEmpty(),
+    check('rating', 'Rating is required').not().isEmpty(),
+], async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (product) {
+            const reviews = product.reviews;
+            const alreadyReviewed = reviews.find(r => r.user.toString() === req.user._id.toString());
+            if (!alreadyReviewed) {
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) {
+                    return res.status(400).json({ errors: errors.array() })
+                }
+                const review = {
+                    name: req.user.name,
+                    rating: Number(req.body.rating),
+                    comment: req.body.comment,
+                    user: req.user._id
+                }
+                product.reviews.push(review);
+                product.numReviews = product.reviews.length;
+                product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+                await product.save();
+                return res.status(200).json({ msg: 'Review added' });
+            }
+            else {
+                return res.status(404).json({ errors: [{ msg: 'Product Already Reviewed' }] });
+            }
+        }
+        else {
+            return res.status(404).json({ errors: [{ msg: 'Product Not Found' }] });
+        }
+    } catch (err) {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ errors: [{ msg: 'Product Not Found' }] });
+        }
+        return res.status(500).json({ errors: [{ msg: 'Internal Server Error' }] });
+    }
+})
 module.exports = router;
